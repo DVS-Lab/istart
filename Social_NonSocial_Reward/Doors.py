5aef29b185202b3d62a67f2c3bd717530f005d26
CalebRHaynes <44538013+CalebRHaynes@users.noreply.github.com>
2019-11-14 15:53:52 -0500

    timing

--- Social_NonSocial_Reward/Doors.py
+++ Social_NonSocial_Reward/Doors.py
@@ -110,7 +110,7 @@ outcome_map = {999: 'You have 3 seconds to respond.'}
 
 
 #instructions
-instruct_screen = visual.TextStim(win, text='In this task, you will see two doors on the computer screen, only one of them will have a prize behind it.  \n \n We want you to tell us which door you think contains a prize. \n \n Press the index finger button to continue.', pos = (0,0), wrapWidth=45, height = 1.2)
+instruct_screen = visual.TextStim(win, text='In this task, you will see two doors on the computer screen, only one of them will have a prize behind it. \n \n We want you to tell us which door you think contains a prize. \n \n Press the index finger button to continue.', pos = (0,0), wrapWidth=45, height = 1.2)
 instruct_screen2 = visual.TextStim(win, text='Press Button 2 (index finger) for the LEFT picture. \n \n Press Button 3 (middle finger) for the RIGHT picture.', pos = (0,0), wrapWidth=45, height = 1.2)
 instruct_screen3 = visual.TextStim(win, text='If you choose correctly, you will see a green arrow pointing up meaning that you won 50 cents.\n \n If you choose incorrectly, you will see a red arrow pointing down, meaning that you lost 25 cents.\n \n If you are not fast enough, the comupter will make a decision for you at random, so make sure you are responding quickly. \n \n Once you see the arrow, that round is over.', pos = (0,0), wrapWidth=45, height = 1.2)
 
