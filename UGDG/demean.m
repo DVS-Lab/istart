73b66dc094ce2893250b1e033e89e2928369ac57
danielsazhin <54955287+danielsazhin@users.noreply.github.com>
2020-02-06 10:47:39 -0500

    update

--- UGDG/demean.m
+++ UGDG/demean.m
@@ -9,7 +9,7 @@ maindir = pwd;
 path = ("pmod/" + "sub-" + subj + "/");
 
 
-data = (path + "sub-" + subj + "run-0" + r + ".bids_cue_ug-resp_parametric.txt");
+data = (path + "sub-" + subj + "run-0" + r + ".bids_cue_ug-resp_parametric_pmod.txt");
 UGResp = importdata(data);
 UGRespMean = mean(UGResp(:,3));
 UGRespDemeaned = UGResp(:,3)-UGRespMean;
@@ -19,7 +19,7 @@ UGResp = [UGResp,UGRespDemeaned];
 output = (path + "sub-" + subj + "run-0" + r + ".bids_cue_ug-resp_parametric_demeaned.txt");
 dlmwrite(output,UGResp,'delimiter','\t','precision',4)
 
-data = (path + "sub-" + subj + "run-0" + r + ".bids_cue_ug-prop_parametric.txt");
+data = (path + "sub-" + subj + "run-0" + r + ".bids_cue_ug-prop_parametric_pmod.txt");
 UGProp = importdata(data);
 UGPropMean = mean(UGProp(:,3));
 UGPropDemeaned = UGProp(:,3)-UGPropMean;
@@ -29,7 +29,7 @@ UGProp = [UGProp,UGPropDemeaned];
 output = (path + "sub-" + subj + "run-0" + r + ".bids_cue_ug-prop_parametric_demeaned.txt");
 dlmwrite(output,UGProp,'delimiter','\t','precision',4)
 
-data = (path + "sub-" + subj + "run-0" + r + ".bids_cue_dict_parametric.txt");
+data = (path + "sub-" + subj + "run-0" + r + ".bids_cue_dict_parametric_pmod.txt");
 Dict = importdata(data);
 DictMean = mean(Dict(:,3));
 DictDemeaned = Dict(:,3)-DictMean;
