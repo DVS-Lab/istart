11f8b8dd5b7795366f3718b5dd360428d4d20950
CalebRHaynes <calebhaynes0@gmail.com>
2021-05-20 17:56:18 -0400

    updating social memory response options

---
+++ social_reward_c/instructions.py
@@ -0,0 +1,71 @@
+#Instruction configuration for Social Doors Task
+
+#Caleb Haynes 5-20-2021
+
+instruction_hello = '''
+Hello! This is the picture task.\n\nPress the index finger button to continue.
+'''
+
+instruction_hello_practice = '''
+Hello! This is a practice for the picture task.
+\n\nPress the index finger (or the number 2 on your keyboard) button to continue.
+'''
+
+instruction1_door = '''
+In this task, you will see two pictures on the computer screen, only one of them will have a prize behind it. 
+\n \nWe want you to tell us which picture you think contains a prize. 
+\n \nPress the index finger (or the number 2 on your keyboard) button to continue.
+'''
+
+instruction1_face = '''
+In this task, you will see two pictures of individuals on the computer screen, one on the left and one on the right. 
+\n \nWe want you to tell us which person you think liked you based on your photo. 
+\n \nPress the index finger (or 2 on your keyboard) button to continue.
+'''
+
+instruction2 = '''
+Press Button 2 (index finger) for the LEFT picture. 
+\n\nPress Button 3 (middle finger) for the RIGHT picture.
+'''
+
+instruction3_door = '''
+If you choose correctly, you will see a green arrow pointing up, meaning that you won 50 cents.
+\n If you choose incorrectly, you will see a red arrow pointing down, meaning that you lost 25 cents.
+\n If you are not fast enough, the computer will make a decision for you at random, so make sure you are responding quickly. 
+\n Once you see the arrow, that round is over.
+'''
+
+instruction3_door_practice = '''
+If you choose correctly, you will see a green arrow pointing up, meaning that you won 50 cents.
+\n If you choose incorrectly, you will see a red arrow pointing down, meaning that you lost 25 cents.
+\n If you are not fast enough, the computer will make a decision for you at random, so make sure you are responding quickly. 
+\nRemember that this is just a practice, these outcomes are based on another participant's choices and will not affect your earnings.  
+\nOnce you see the arrow, that round is over.
+'''
+
+instruction3_face = '''
+If you choose correctly, you will see a green arrow pointing up, meaning that you chose the person who said they liked you.
+\nIf you choose incorrectly, you will see a red arrow pointing down, meaning that you did not choose the person who said they liked you;
+that person actually disliked you.
+\nIf you are not fast enough, the computer will make a decision for you at random, so make sure you are responding quickly. 
+\nOnce you see the arrow, that round is over.
+'''
+
+instruction3_face_practice = '''
+If you choose correctly, you will see a green arrow pointing up, meaning that you chose the person who said they liked you.
+\nIf you choose incorrectly, you will see a red arrow pointing down, meaning that you did not choose the person who said they liked you;
+that person actually disliked you.
+\nIf you are not fast enough, the computer will make a decision for you at random, so make sure you are responding quickly. 
+\nRemember that this is just a practice, these outcomes are based on another participant's ratings and not those based on your photo.   
+\nOnce you see the arrow, that round is over.
+'''
+
+ready_screen = '''
+Please wait for the game to begin! 
+\n\nRemember to keep your head still!
+'''
+
+ready_screen_practice = '''
+Please wait for the practice game to begin! 
+\n\nRemember to keep your head still!
+'''
